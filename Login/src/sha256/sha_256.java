fffd5eb4b9b7590fc6a90fc8436e913dec86a24b
ComeOnKissMe <39038790+ComeOnKissMe@users.noreply.github.com>
2018-06-04 10:15:18 +0800

    javaweb

---
+++ Login/src/sha256/sha_256.java
@@ -0,0 +1,50 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class sha_256 {
+
+	/**
+	 * 利用java原生的摘要实现256加密
+	 * @param str 加密后的报文
+	 * @param args
+	 */
+	public static String getSHA256Strjava(String str) {
+		MessageDigest messageDigest = null;
+		String encodeStr = "";
+		try {
+			try {
+				messageDigest =MessageDigest.getInstance("SHA-256");
+				messageDigest.update(str.getBytes("UTF-8"));
+				encodeStr =byte2Hex(messageDigest.digest());
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return encodeStr;
+	}
+	
+	private static String byte2Hex(byte[] bytes) {
+		// TODO Auto-generated method stub
+		StringBuffer stringBuffer = new StringBuffer();
+		String temp = null;
+		for (int i = 0;i<bytes.length;i++) {
+			temp = Integer.toHexString(bytes[i] & 0xFF);
+			if(temp.length()==1) {
+				stringBuffer.append("0");
+			}
+			stringBuffer.append(temp);
+		}
+		return stringBuffer.toString();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(getSHA256Strjava("admin"));
+	}
+
+}
