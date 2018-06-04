a6fe45f3841cc6d68bab408a0d3d9a49369970c4
ComeOnKissMe <39038790+ComeOnKissMe@users.noreply.github.com>
2018-06-04 10:16:31 +0800

    javaweb

--- Login/src/servlet/loginservlet.java
+++ Login/src/servlet/loginservlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import factory.Daofactory;
+import vo.User;
 
 
 public class loginservlet extends HttpServlet{
@@ -15,6 +20,35 @@ public class loginservlet extends HttpServlet{
     	String pass = request.getParameter("passs");
     	String kaptcha = (String)request.getSession().getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY);
     	String yzminput = request.getParameter("yzminput");
-    	
+    	List<String> info = new ArrayList<String>(); 
+    	if(yzminput == null || !yzminput.equalsIgnoreCase(kaptcha)) {
+    		info.add("验证码错误！<a hred =login.jsp>点击返回</a>");
+    	}
+    	if(name == null || "".equals(name)) {
+    		info.add("用户名不能为空!<a href = login.jsp>点击返回</a>");
+    	}
+    	if(pass == null || "".equals(pass)) {
+    		info.add("密码不能为空！<a> hred = login.jsp>点击返回</a>");
+    	}
+    	if(info.size()==0) {
+    		User user = new User();
+    		user.setUsername(name);
+    		user.setPassword(pass);
+    		try {
+				if(Daofactory.getUserDaoInstance().findLogin(user)) {
+				info.add("用户登录成功，欢迎"+user.getUsername()+"光临！");
+				}else {
+					info.add("用户登录失败，错误的用户名和密码！")；
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	request.setAttribute("info", info);
+    	request.getRequestDispatcher(path).forward(request, response);
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+    	this.doGet(request, response);
     }
 }
